72b8fb9e45bd482d5e390700c5db43a7cb36b555
MD ARIF <arifahmad207@gmail.com>
2023-08-05 20:18:18 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import postsReducer from './reducers';
-
-const store = createStore(postsReducer, applyMiddleware(thunk));
-
-export default store;
